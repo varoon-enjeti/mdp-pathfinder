d8b8984024e29a6353219585dc69b9a8c1ec8fc7
Varoon Enjeti <97761722+varoon-enjeti@users.noreply.github.com>
2023-05-18 14:36:23 -0500

    adding some comments to mdp

--- src/mdp.py
+++ src/mdp.py
@@ -1,28 +1,29 @@
-# direction_probability * (-1*power_cost + (discount * values[y][x]))
-# direction_probability * (-2*power_cost + (discount * values[y][x]))
+
+
 
 # Tie-Breaking Rules:
 # normal speed, boosted speed
 # south, west, north, east
 
-
-def copy_values(values, n):
-    new = [[0 for j in range(6)] for i in range(n)]
-    for y in range(n):
-        for x in range(n):
+# Copies values from one nxn matrix to another, in order to prevent copying by reference
+def copy_values(values, size):
+    new = [[0 for j in range(6)] for i in range(size)]
+    for y in range(size):
+        for x in range(size):
             new[y][x] = values[y][x]
     return new
 
-
-def converges(prev, curr):
-    for y in range(n):
-        for x in range(n):
+# Checks to see if the previous and current value boards converge by a factor of 0.1%
+def converges(prev, curr, size):
+    for y in range(size):
+        for x in range(size):
             if abs(prev[y][x] - curr[y][x]) > 0.001:
                 return False
     return True
 
-
+# Calculates the next possible 8 moves and then sets values to the max of that move and policies to the direction of the max move
 def calc_next_moves(curr_posn, values, policies, power_cost, discount, size):
+    # Determine if neighboring positions are in range of the map
     s_range = True
     w_range = True
     n_range = True
@@ -37,6 +38,7 @@ def calc_next_moves(curr_posn, values, policies, power_cost, discount, size):
     if curr_posn[1]+1 > (size-1):
         e_range = False
 
+    # Based on if the neighboring positions are in range, calculate the viable neighboring positions
     s_posn = ()
     w_posn = ()
     n_posn = ()
@@ -62,6 +64,8 @@ def calc_next_moves(curr_posn, values, policies, power_cost, discount, size):
     else:
         e_posn = curr_posn
 
+    # Calculate the utility at all neighboring positons while using no speed boost
+    # direction_probability * (-1 * power_cost + (discount * values[next[y]][next[x]]))
     s = (0.7 * (-1 * power_cost + (discount * values[s_posn[0]][s_posn[1]]))) + \
         (0.15 * (-1 * power_cost + (discount * values[w_posn[0]][w_posn[1]]))) + \
         (0.15 * (-1 * power_cost +
@@ -82,6 +86,8 @@ def calc_next_moves(curr_posn, values, policies, power_cost, discount, size):
         (0.15 * (-1 * power_cost +
          (discount * values[s_posn[0]][s_posn[1]])))
 
+    # Calculate the utility at all neighboring positions while using speed boost
+    # direction_probability * (-2 * power_cost + (discount * values[next[y]][next[x]]))
     ss = (0.8 * (-2 * power_cost + (discount * values[s_posn[0]][s_posn[1]]))) + \
         (0.1 * (-2 * power_cost + (discount * values[w_posn[0]][w_posn[1]]))) + \
         (0.1 * (-2 * power_cost +
@@ -101,25 +107,34 @@ def calc_next_moves(curr_posn, values, policies, power_cost, discount, size):
         (0.1 * (-2 * power_cost + (discount * values[n_posn[0]][n_posn[1]]))) + \
         (0.1 * (-2 * power_cost +
          (discount * values[s_posn[0]][s_posn[1]])))
-
-    curr_val = values[curr_posn[0]][curr_posn[1]]
+        
+    # Add all possible 8 moves to an list and find the move with the maximum utility
     moves = [s, w, n, e, ss, ww, nn, ee]
     max_val = max(moves)
     max_move = moves.index(max_val)
 
+    # Set values at the current position to the maximum move value and policies at the current position to the maximum move direction
     values[curr_posn[0]][curr_posn[1]] = max_val
     policies[curr_posn[0]][curr_posn[1]] = max_move+1
 
 
-# map : 6x6 
-def robot_travel_planner(map, policies, values, delivery_price, power_cost, robotrepair_cost, discount, n):
+# map : nxn matrix containing the positions of the start, goal, and hazard positions (denoted with 1,2,3 respectively)
+# policies : empty nxn matrix that is updated with the best possible move after the function call
+# values : empty nxn matrix that is updated with the utility value at each position on the board after the function call
+# delivery_price : the utility earned for completing the robot delivery
+# power_cost : the cost of power by the robot at each step
+# robot_repair_cost : the negative utility that is earned when the robot ends up in a hazardous position
+# discount : the discount at each step in the operation (gamma value)
+# size : the n value for the nxn matrices provided by map, policies, and values
+# --> returns the utility value at the start position
+def robot_travel_planner(map, policies, values, delivery_price, power_cost, robot_repair_cost, discount, size):
     # Find Start, Goal, & Hazard Positions
-    prev = copy_values(values, n)
+    prev = copy_values(values, size)
     start = ()
     goal = ()
     rivals = []
-    for y in range(n):
-        for x in range(n):
+    for y in range(size):
+        for x in range(size):
             if map[y][x] == 1:
                 start = (y, x)
             if map[y][x] == 2:
@@ -130,7 +145,7 @@ def robot_travel_planner(map, policies, values, delivery_price, power_cost, robo
     # Initialize Values with Rewards at Goal & Hazard Positions
     values[goal[0]][goal[1]] = delivery_price
     for r in rivals:
-        values[r[0]][r[1]] = robotrepair_cost*-1
+        values[r[0]][r[1]] = robot_repair_cost*-1
         
     # Loop Until Board Converges
     while True:
@@ -139,10 +154,10 @@ def robot_travel_planner(map, policies, values, delivery_price, power_cost, robo
                 curr_posn = (y, x)  
                 if (curr_posn != goal) and (curr_posn not in rivals):
                     calc_next_moves(curr_posn, values,                  # Calculate the next moves for positions...
-                                    policies, power_cost, discount, n)     # ...that are not goal or hazard positions
-        if converges(prev, values):         # Stop looping and return if board converges
+                                    policies, power_cost, discount, size)     # ...that are not goal or hazard positions
+        if converges(prev, values, size):         # Stop looping and return if board converges
             break
-        else:                               # Else, continue looping and keep track of previous values 2D Array
-            prev = copy_values(values, n)
+        else:                               # Else, continue looping and keep track of previous values nxn matrix
+            prev = copy_values(values, size)
 
-    return values[start[0]][start[1]]
+    return values[start[0]][start[1]]   # Returns utility values at the start position
